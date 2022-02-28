2a69cd41a52849a27fed2c71ed1b81fb0ae9d31d
fbtwitter <rzaugusdi354@gmail.com>
2022-02-28 20:03:40 +0700

    update readme

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import qrImage from './assets/images/qr-code.png';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <main class="Main">
+  <main>
     <section>
       <div class="wrapper">
         <div class="card">
           <figure>
-            <img src="${qrImage}"/>
+            <img src="${qrImage}" alt="qr code"/>
             <figcaption>
               <h1>Improve your front-end skills by building projects</h1>
               <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
